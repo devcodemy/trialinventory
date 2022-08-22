c3795ac950f29fde5272ec663d9eac1c9b2419af
devcodemy <devcodemy@gmail.com>
2022-08-22 23:03:30 +0300

    add health

--- restapi/main.go
+++ restapi/main.go
@@ -1,5 +1,21 @@
 package restapi
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func HealthCheck() string {
 	return "UP"
+}
+
+func Com() {
+	rt := gin.Default()
+	rt.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "UP",
+		})
+	})
+	rt.Run("localhost:8080")
 }
